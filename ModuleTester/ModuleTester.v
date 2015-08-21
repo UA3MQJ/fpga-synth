92d811229cdce6ec13b5d503d491793c69369599
Alex <ua3mqj@gmail.com>
2015-08-21 23:06:15 +0300

    version of note mono

--- ModuleTester/ModuleTester.v
+++ ModuleTester/ModuleTester.v
@@ -1,29 +1,7 @@
-module ModuleTester(clk, clken, reset_n, in_data, in_valid, out_ready, in_error, out_data, in_ready, out_valid, out_error);
+module ModuleTester(clk);
 
-	input		clk;
-	input		clken;
-	input		reset_n;
-	input		in_data;
-	input		in_valid;
-	input		out_ready;
-	input	[1:0]	in_error;
-	output	[15:0]	out_data;
-	output		in_ready;
-	output		out_valid;
-	output	[1:0]	out_error;
 
-  cic1 cic1_inst (
-      .clk(clk),
-      .clken(1),
-      .reset_n(1),
-      .in_ready(in_ready),
-      .in_valid(1),
-      .in_data(in_data),
-      .out_data(out_data),
-      .in_error(2'b00),
-      .out_error(out_error),
-      .out_ready(1),
-      .out_valid(out_valid)
-      );
+
+
 
 endmodule
---
+++ modules/note_mono_harray.v
@@ -0,0 +1,63 @@
+module note_mono_harray(clk, rst, note_on, note_off, note, out_note, out_gate);			   				   
+
+//inputs outputs
+input wire clk, rst, note_on, note_off;
+input wire [6:0] note;
+output wire [6:0] out_note;
+output reg out_gate;
+
+initial out_gate <= 0;
+
+reg [6:0] t_out_note;
+initial t_out_note <= 7'd0;
+
+assign out_note = t_out_note;//(out_gate) ? t_out_note : 7'd0;
+
+reg [127:0] keys;
+initial keys <= 128'd0;
+reg [6:0] bit_ptr;
+
+//state list
+parameter READY  = 1'd0;
+parameter BUSY   = 1'd1;
+reg state;
+initial state <= READY;
+
+always @(posedge clk) begin
+	if (rst) begin
+		out_gate <= 0;
+		t_out_note <= 7'd0;
+		keys <= 128'd0;
+		state <= READY;
+	end else if (state==READY) begin
+		if (note_on) begin
+			keys[note] <= 1;
+			bit_ptr <= 7'd127;
+			state <= BUSY;
+		end else if (note_off) begin
+			keys[note] <= 0;
+			bit_ptr <= 7'd127;
+			state <= BUSY;
+		end
+	end else if (state==BUSY) begin
+		if (note_on||note_off) begin //если в процессе работы еще что-то пришло
+			if (note_on) begin
+				keys[note] <= 1;
+			end else if (note_off) begin
+				keys[note] <= 0;
+			end
+			bit_ptr <= 7'd127;
+		end else if (bit_ptr==7'd0) begin //не нашли ни одной установленной ноты
+			out_gate <= 0;
+			state <= READY;
+		end else if (keys[bit_ptr]== 1'b1) begin //нашли установленый бит - заканчиваем поиск
+			out_gate <= 1;
+			t_out_note <= bit_ptr;
+			state <= READY;
+		end else begin
+			bit_ptr <= bit_ptr - 1'b1;
+		end
+	end
+end
+
+endmodule
